117a3bf89db4fd0841b297839632a83870afc5b1
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-25 20:01:08 +0300

    Сделал 2-е

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    /></a>`;
+    })
+    .join("");
+}
+
+const container = document.querySelector(".gallery");
+const galleryList = createGallery(galleryItems);
+container.insertAdjacentHTML("beforeend", galleryList);
+console.log(container);
+
+const lightbox = new SimpleLightbox(".gallery a", { captionType: "alt", captionsData: "alt", captionPosition: "bottom", captionDelay: "250" });
+
+// container.addEventListener("click", onGetOriginalImg);
+
+// function onGetOriginalImg(evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+//   instance.show();
+// }
