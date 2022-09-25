02fa247dc708f51bd98d3a9ca069ac39b933f27b
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-25 18:45:28 +0300

    Сделал 1-е

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`;
+    })
+    .join("");
+}
+
+const container = document.querySelector(".gallery");
+const galleryList = createGallery(galleryItems);
+container.insertAdjacentHTML("beforeend", galleryList);
+console.log(container);
+
+container.addEventListener("click", onGetOriginalImg);
+
+function onGetOriginalImg(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modal = basicLightbox.create(`<img src="${evt.target.dataset.source} width="800" height="600">`);
+  modal.show();
+}
+
+// document.querySelector("a.gallery__link").onclick = () => {
+//   basicLightbox
+//     .create(
+//       `
+// 		<img src="${original}">
+// 	`
+//     )
+//     .show();
+// };
